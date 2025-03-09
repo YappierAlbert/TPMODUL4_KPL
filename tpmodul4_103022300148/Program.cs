525dc725d2277b4a98d2ab6f7d3ead6c591de880
Yappier Albertus <yappier.xrpl37@gmail.com>
2025-03-10 00:22:34 +0700

    Add files via upload

--- tpmodul4_103022300148/Program.cs
+++ tpmodul4_103022300148/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class KodePos()
+{
+    public void GetKodePos(String input)
+    {
+        String[] kelurahan = { "Batununggal", "Kujangsari", "Mengger", "Wates",
+            "Cijaura", "Jatisari", "Margasari", "Sekejati", "Kebonwaru", "Maleer", "Samoja" };
+        int[] kode = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286, 40272, 40274, 40273 };
+
+        for (int i = 0; i < kelurahan.Length; i++)
+        {
+            if (kelurahan[i] == input)
+            {
+                Console.WriteLine("Kode Pos : " + kode[i]);
+            }
+        }
+    }
+}
+
+public class Program()
+{
+    public static void Main()
+    {
+        KodePos pos = new KodePos();
+        Console.Write("Masukkan Kode Pos : ");
+        String input = Console.ReadLine();
+        pos.GetKodePos(input);
+    }
+}
